458d13bd9a4641f3618c945a4645cef005807d36
Luu Truong Sinh <Luu Truong Sinh>
2017-01-03 23:53:06 +0700

    remove simulator folder

--- Candy_Run/simulator/win32/src/objects/Ground.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by SinhBlack on 12/30/2016.
- */
-var Ground = ObjectGame.extend({
-    ctor:function () {
-        this._super();
-    }
-});
--- Candy_Run/simulator/win32/src/scenes/LayerPlayStatus.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Fresher on 12/29/2016.
- */
-
-
-var LayerStatus= cc.Layer.extend({
-
-});
