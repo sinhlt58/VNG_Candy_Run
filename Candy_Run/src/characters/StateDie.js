3503071c7f55f088140510321a5d6c5c01b8be8c
Vũ Hồng Phú <magicghost.vu@gmail.com>
2017-01-13 16:01:16 +0700

    fix logic die and reborn

--- Candy_Run/src/characters/StateDie.js
+++ Candy_Run/src/characters/StateDie.js
@@ -1,21 +1,70 @@
 /**
  * Created by Fresher on 13/01/2017.
  */
-var StateDie= StateMovement.extend({
+var StateDie = StateMovement.extend({
+
+
+    duration: null,
+
+    timePass: null,
+
+
+    grounded: null,
+
+    setAnimation: null,
+
     ctor: function () {
+        this.duration = 1;
+
+
+        this.timePass = 0;
+        this.grounded = false;
+        this.setAnimation=false;
 
     },
     update: function (dt, character) {
+        if (character.getPosition().y <= 90) {
+            this.grounded = true;
+        }
+        if (this.grounded = true) {
+            this.timePass += dt;
+            character.setPositionY(90);
+            character.setAccelerationY(0);
+
+            // not play die animation
+            if(this.setAnimation==false){
+                character.animationController.setAnimation("die", false);
+            }
+            // animation was set
+            else{
+
+            }
+
+
+
+
+        }
+
 
     },
     onEnter: function (character) {
-        character.animationController.setAnimation('die', false);
+        //character.animationController.setAnimation('die', false);
+
+        //stop the character
         character.setVelocityX(0);
+
+
+
+        // make character fall
+        if (character.getPosition().x > 90) {
+            this.character.setAccelerationY(-300);
+        }
+
+
     },
     onExit: function (character) {
 
     }
-
 
 
 });
--- Candy_Run/src/characters/StateReborn.js
+++ Candy_Run/src/characters/StateReborn.js
@@ -1,8 +1,35 @@
 /**
  * Created by Fresher on 13/01/2017.
  */
-var StateReborn= StateMovement.extend({
-    onEnter: function () {
-        
+
+// can only move to this state from die
+    // after duration, character will come back to running
+var StateReborn = StateMovement.extend({
+
+    duration: null,
+    timePass: null,
+
+
+    ctor: function () {
+        this.duration = 1;
+        this.timePass = 0;
+
+    },
+    onEnter: function (character) {
+        character.animationController.setAnimation("reborn", false);
+
+
+    },
+    onExit: function (character) {
+
+    },
+    update: function (dt, character) {
+        this.timePass+=dt;
+        if(this.timePass>=this.duration){
+            character.stateMachine.changeState("stateMovement", new StateRunning());
+        }
+
+
+
     }
 });
